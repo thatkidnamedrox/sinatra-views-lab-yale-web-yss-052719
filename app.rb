3206a8741cd46127cb25eec7150182f412039f24
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-24 18:35:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,12 +5,18 @@ class App < Sinatra::Base
 	end
 
 	get '/goodbye' do
+<<<<<<< HEAD
 		@name = "Joe"
+=======
+>>>>>>> 98038f015e64b1602a624fc8b480fab4fe482228
 		erb :goodbye
 	end
 
 	get '/date' do
+<<<<<<< HEAD
 		@date = DateTime.now
+=======
+>>>>>>> 98038f015e64b1602a624fc8b480fab4fe482228
 		erb :date
 	end
 
